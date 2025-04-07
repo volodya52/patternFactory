2bc0ddc2cce9533cd53e87e8172357bc65241da0
student3 <shurkov2005@gmail.com>
2025-04-07 10:02:34 +0500

    fc

---
+++ CoffeClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee
+{
+    public class CoffeClass
+    {
+        public void GrindCoffee ()
+        {
+            Console.WriteLine("Перемалываем кофе");
+        }
+        public void MakeCoffee ()
+        {
+            Console.WriteLine("Делаем кофе");
+        }
+        public void PourIntoCup ()
+        {
+            Console.WriteLine("Наливаем кофе в стакан");
+        }
+        public CoffeClass OrderCoffee (CoffeeType type)
+        {
+            CoffeClass coffee = null;
+            switch (type)
+            {
+                case CoffeeType.AMERICANO:
+                    coffee = new Americano( );
+                    break;
+                case CoffeeType.ESPRESSO:
+                    coffee = new Espresso( );
+                    break;
+                case CoffeeType.CAPPUCINO:
+                    coffee = new Espresso( );
+                    break;
+                case CoffeeType.CAFFEE_LATTE:
+                    coffee = new CaffeLatte( );
+                    break;
+            }
+            Console.WriteLine($"Вы заказали - {type}");
+            coffee.GrindCoffee( );
+            coffee.MakeCoffee( );
+            coffee.PourIntoCup( );
+            Console.WriteLine("Вот ваш кофе, приходите к нам еще!!))");
+            return coffee;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee
+{
+
+    internal class Program
+    {
+        
+        static void Main (string [ ] args)
+        {
+            CoffeClass coffee = new CoffeClass( );
+            CoffeeType type = new CoffeeType( );
+            type = CoffeeType.CAPPUCINO;
+            coffee.OrderCoffee(type);
+            Console.ReadKey( );
+        }
+
+        
+
+        
+    }
+}
