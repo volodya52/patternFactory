03bfd8d0552b6446c7a5639a5599dff532af5d39
student3 <shurkov2005@gmail.com>
2025-04-07 10:18:59 +0500

    sc

--- Program.cs
+++ Program.cs
@@ -12,15 +12,34 @@ namespace Coffee
         
         static void Main (string [ ] args)
         {
-            CoffeClass coffee = new CoffeClass( );
-            CoffeeType type = new CoffeeType( );
-            type = CoffeeType.CAPPUCINO;
-            coffee.OrderCoffee(type);
-            Console.ReadKey( );
+            while (true)
+            {
+                CoffeClass coffee = new CoffeClass( );
+                CoffeeType type = new CoffeeType( );
+                Console.WriteLine("Выберите кофе:\n1)Americano\n2)Latte\n3)Cappucino\n4)Espresso");
+                int c = Convert.ToInt32(Console.ReadLine( ));
+                if (c == 1)
+                {
+                    type = CoffeeType.AMERICANO;
+                    coffee.OrderCoffee(type);
+                }
+                else if (c == 2)
+                {
+                    type = CoffeeType.CAFFEE_LATTE;
+                    coffee.OrderCoffee(type);
+                }
+                else if (c == 3)
+                {
+                    type = CoffeeType.CAPPUCINO;
+                    coffee.OrderCoffee(type);
+                }
+                else if (c == 4)
+                {
+                    type = CoffeeType.ESPRESSO;
+                    coffee.OrderCoffee(type);
+                }
+                Console.ReadKey( );
+            }
         }
-
-        
-
-        
     }
 }
